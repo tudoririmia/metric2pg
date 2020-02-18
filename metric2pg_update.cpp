27baea014afc0625fbaa8b932990f50bdc4cd258
tudor.irimia <tudor.irimia@outlook.com>
2020-02-18 10:20:25 -0800

    create/fetch/update arguments

--- metric2pg_update.cpp
+++ metric2pg_update.cpp
@@ -3,9 +3,13 @@
 #include "metric2pg.hpp"
 using namespace std;
 
-int metric2pg_update () {
+int metric2pg_update (int argc, char** argv) {
 
     std::cout << "Not implemented yet" << std::endl;
+    std::cout << "The arguments are: " << std::endl;
+    for (int i = 2; i < argc; i++) {
+        std::cout << argv[i] << std::endl;
+    }
     return 0;
 
 }
