8aaaf6478eaedae381545282e18b815f85264599
tudor.irimia <tudor.irimia@outlook.com>
2020-02-22 08:19:23 -0800

    print args using list function

--- metric2pg_create.cpp
+++ metric2pg_create.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <string>
+#include <list>
+#include <iterator>
 #include "metric2pg.hpp"
 using namespace std;
 
-int metric2pg_create (int argc, char**argv) {
+
+int metric2pg_create (std::list <char*> arguments) {
 
     std::cout << "Not implemented yet" << std::endl;
     std::cout << "The arguments are: " << std::endl;
-    for (int i = 2; i < argc; i++) {
-        std::cout << argv[i] << std::endl;
-    }
+    showlist(arguments);
     return 0;
 
 }
--- metric2pg_fetch.cpp
+++ metric2pg_fetch.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <string>
+#include <list>
+#include <iterator>
 #include "metric2pg.hpp"
 using namespace std;
 
-int metric2pg_fetch (int argc, char** argv) {
+int metric2pg_fetch (std::list <char*> arguments) {
 
     std::cout << "Not implemented yet" << std::endl;
     std::cout << "The arguments are: " << std::endl;
-    for (int i = 3; i < argc; i++) {
-        std::cout << argv[i] << std::endl;
-    }
+    showlist(arguments);
+    
     return 0;
-
 }
