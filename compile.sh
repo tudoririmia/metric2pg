5b94142385bba47b86e4c5ec41566c6dbdf91a85
tudoririmia <tudor_irimia@outlook.com>
2020-07-01 16:47:41 +0300

    added drop for compiling

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-g++ metric2pg.cpp metric2pg_create.cpp metric2pg_fetch.cpp metric2pg_update.cpp -Wall -lpqxx -lpq -lboost_program_options -o metric2pg
+g++ metric2pg.cpp metric2pg_create.cpp metric2pg_fetch.cpp metric2pg_update.cpp metric2pg_drop.cpp -Wall -lpqxx -lpq -lboost_program_options -o metric2pg
 
