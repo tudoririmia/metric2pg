63922f016ff58a56912feeb0ff03c72b03d02a84
tudoririmia <tudor_irimia@outlook.com>
2020-07-01 16:49:53 +0300

    insert to database

--- metric2pg_update.cpp
+++ metric2pg_update.cpp
@@ -1,15 +1,62 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <list>
+#include <pqxx/pqxx>
 #include <iterator>
 #include "metric2pg.hpp"
 using namespace std;
+using namespace pqxx;
+
 
 int metric2pg_update (std::list <char*> arguments) {
 
-    std::cout << "Not implemented yet" << std::endl;
-    std::cout << "The arguments are: " << std::endl;
-    showlist(arguments);
+    char* fst = arguments.front();
+    
+    std::list <char*> tokenized = tokenize(fst);
+
+    char* table = tokenized.back();
+    tokenized.pop_back();
+    char* database = tokenized.back();
+    tokenized.pop_back();
+    char* user = tokenized.front();
+    tokenized.pop_front();
+    char* pass = tokenized.back();
+    tokenized.pop_back();
+
+    string str1 = "dbname = " + std::string(database) + " ";
+    string str2 = str1 + "user = " + std::string(user) + " ";
+    string str3 = str2 + "password = " + std::string(pass) + " ";
+    string conn_string = str3 + "hostaddr = 127.0.0.1 port = 5432";
+
+    arguments.pop_front();
+    char* value = arguments.front();
+
+    try {
+      connection C(conn_string);
+      if (C.is_open()) {
+         cout << "Connection OK" << endl;
+      } else {
+         cout << "Can't open database" << endl;
+         return 1;
+      }
+
+      /* Create SQL statement */
+      string sql = "INSERT INTO " + std::string(table) + "(time, temperature)" \
+                   "VALUES(NOW()," + std::string(value) + ");";
+
+      /* Create a transactional object. */
+      work W(C);
+      
+      /* Execute SQL query */
+      W.exec( sql );
+      W.commit();
+      cout << "Table updated successfully with value: " << value << endl;
+      C.disconnect ();
+   } catch (const std::exception &e) {
+      cerr << e.what() << std::endl;
+      return 1;
+   }
+
     
     return 0;
 
